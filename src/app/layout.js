5b88a33a8b3a391b880852148f5bd39fc826e833
Camalu Atacado <camaluatacado@gmail.com>
2025-01-13 12:03:11 -0300

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 import { RenavamProvider } from "../contexts/RenavamContext";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <RenavamProvider>{children}</RenavamProvider>
+        <Analytics />
       </body>
     </html>
   );
